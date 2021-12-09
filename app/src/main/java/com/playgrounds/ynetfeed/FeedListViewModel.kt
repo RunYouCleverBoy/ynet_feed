6ab150d9ceba1f9f14a40d00e96323e9f1b262c8
Shmuel Meymann <smeymann@lightricks.com>
2021-12-09 02:47:32 +0200

    Scope responsibility instead of endless loop

--- app/src/main/java/com/playgrounds/ynetfeed/FeedListViewModel.kt
+++ app/src/main/java/com/playgrounds/ynetfeed/FeedListViewModel.kt
@@ -3,9 +3,11 @@ package com.playgrounds.ynetfeed
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.playgrounds.ynetfeed.models.PostItemInfo
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
 
 class FeedListViewModel(app: Application) : AndroidViewModel(app) {
     private val repository = FeedRepository(app)
@@ -18,13 +20,15 @@ class FeedListViewModel(app: Application) : AndroidViewModel(app) {
     val busyFlow = busyFlowMutable.asStateFlow()
 
     suspend fun requestFeedFor(uri: String, minPeriod: Long) {
-        while (true) {
-            ongoingUris.add(uri)
-            busyFlowMutable.emit(true)
-            resultsFlowMutable.emit(repository.request(uri))
-            ongoingUris.remove(uri)
-            busyFlowMutable.tryEmit(ongoingUris.isNotEmpty())
-            delay(minPeriod)
+        coroutineScope {
+            while (isActive) {
+                ongoingUris.add(uri)
+                busyFlowMutable.emit(true)
+                resultsFlowMutable.emit(repository.request(uri))
+                ongoingUris.remove(uri)
+                busyFlowMutable.tryEmit(ongoingUris.isNotEmpty())
+                delay(minPeriod)
+            }
         }
     }
 
